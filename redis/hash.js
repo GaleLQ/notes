e527e7733fdc0d83f9d6f8e6fe6e8bdf7d64fcee
gale <1372363493@qq.com>
2019-10-29 15:14:18 +0800

    auto commit

---
+++ redis/hash.js
@@ -0,0 +1,15 @@
+// hset hash field value 将hash中field设置为value
+// hsetnx hash field value 当不存在时
+// hget hash field 返回hash中给定的field的值
+// hexists hash field 检查field是否存在
+// hdel hash field [field] 删除hash中的field
+// hlen hash 返回域的数量
+// hstrlen hash field 返回相应域的长度
+// hincrby hash field increment 相应值增加整数
+// hincrbyfloat hash field increment 浮点
+// hmset hash field value [field value] 批量设置
+// hmget hash field [field] 批量get
+// hkeys hash 返回所有域
+// hvals hash 返回所有值
+// hgetall 返回所有域值
+// hscan
