c9cb39e3a2f1e9e08eb2e61f4d4a1baa502ba419
gale <1372363493@qq.com>
2019-12-12 21:56:58 +0800

    auto commit

--- mongodb/operators.js
+++ mongodb/operators.js
@@ -1,6 +1,7 @@
 // 注意字段不存在时,各个操作符的情况
 // 大多数情况下都是针对字段值或者字段数组中的值判断是否符合要求
 // 多个操作符嵌套使用注意兼容情况
+// 注意mongodb限制的存储大小
 
 // Query
 // Comparison
@@ -28,7 +29,7 @@
 // $type 检验字段的类型 // TODO MinkeyMaxkey待完善
 
 // Evaluation
-// $expr { $expr: { <expression> } }查询时使用聚合表达式 // TODO
+// $expr { $expr: { <expression> } }使查询时也可使用聚合表达式 // TODO
 // $jsonSchema { $jsonSchema: <JSON Schema object> } 在insert和update时强制验证.作为查询条件操作符
 // $mod { field: { $mod: [ divisor, remainder ] } } 模运算查询
 // $regex 正则,注意前缀优化,非大小写优化
@@ -88,13 +89,32 @@
 // Aggregation
 // Stages
 // $addFields 添加新字段(可通过现有字段)
-// $bucket (aggregation) 将集合分组处理输出(groupBy, boundaries, default, output,  $facet)
+// $bucket (aggregation) 将集合根据某字段范围分组处理输出(groupBy, boundaries, default, output,  $facet)
 // $bucketAuto 自动分成若干组进行处理输出
 // $collStats (aggregation)
-// $count 文档的数量
-// $currentOp (aggregation) 数据库
+// $count(aggregation) 文档的数量
+// $currentOp (aggregation) 数据库当前操作语句信息
 // $facet (aggregation) 同一输入执行多个aggregation
 // $geoNear (aggregation) 距离指定点从近到远输出
 // $graphLookup (aggregation) 递归嵌套搜索并且按照关系返回(如单表自查询)
-
-// 
+// $group(aggregation) 将集合根据某字段相同值分组处理输出(即groupby,注意与$bucket区别),$$ROOT表示当前的document对象
+// $indexStats(aggregation) 索引的统计信息(ops和since)
+// $limit(aggregation) 限制数量,和sort时会优化
+// $listLocalSessions(db) 缓存在内存中的sessions
+// $listSessions(db) 数据库中的sessions(use config db.system.sessions.aggregate)
+// $lookup(aggregation) 表连接,undefined和null也会连接, pipeline自定义不相关查询(嵌套子查询)
+// $match(aggregation) 过滤数据
+// $merge(aggregation) 将结果写入集合的部分(按需更新等)
+// $out(aggregation) 将结果写入或代替集合(完全)
+// $planCacheStats(db) 返回集合缓存相关统计信息
+// $project (aggregation) 映射, 可增加,调整,修改,重组字段等强大功能,$$REMOVE配合$cond可用于字段的部分删除
+// $redact(aggregation) 对文档型内容进行过滤,常配合$cond($$DESCEND保留当前并继续,$$PRUNE排除当前及其嵌套,$$KEEP保留当前及其嵌套)
+// $replaceRoot(aggregation) 用新document代替当前document,(通过默认值,使文档规范等)
+// $replaceWith(aggregation) 上者别名,语法更简洁
+// $sample(aggregation) 随机抽取指定数量样本
+// $set(aggregation) 同$addFields(4.2开始),也相等于显式的$project
+// $skip(aggregation) 跳过指定数量documents
+// $sort(aggregation) 排序(limit自动优化, 100M内存, 考虑尽量前置索引优化)
+// $sortByCount(aggregation) 按照分组求和的数量降序排序($group $sort可代替)
+// $unset(aggregation) 移除一个或多个字段,相同于隐式的$project
+// $unwind (aggregation) 解构数组字段作为当前字段
