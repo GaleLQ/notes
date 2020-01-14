239dc87ca512c6064cfb4f57a0a601902e6a097b
gale <1372363493@qq.com>
2020-01-14 09:49:33 +0800

    auto commit

---
+++ javascript/Function.js
@@ -0,0 +1,21 @@
+// 函数无this关键字,arguments.callee指向本身(严格模式不可用)
+// 函数名.length是定义参数个数
+
+// 箭头函数
+// 没有自己的this，arguments，super或new.target
+// 没有prototype
+
+// 默认参数值
+
+// 方法的定义
+
+// 剩余参数
+
+// Arguments 对象
+// 类数组对象(只有length无其他方法)
+// 可以通过索引直接更改原参数
+
+// getter 将对象属性绑定到函数
+// 懒计算
+// 可动态属性参数
+// setter
--- javascript/语句和声明.js
+++ javascript/语句和声明.js
@@ -12,3 +12,17 @@
 // for
 // for await...of await一下可迭代对象每个值(常用异步生成器)
 // for...in 遍历对象和其原型链上除Symbol的可枚举属性(不保证键顺序),Object.getOwnPropertyNames自己全部,Object.keys自己可枚举
+// for...of在可迭代对象上根据定义创建一个迭代循环(重复使用生成器无效)
+// function 注意整体提升
+// function*
+// if...else
+// import 浏览器端需要script标签type为module
+// import.meta 暴露特定上下文的元数据属性的对象(现有url)
+// label 一条语句前面加个可以引用的标识符(配合continue和break)
+// let
+// return 结尾自动插入分号
+// switch case
+// throw 抛出一个用户自定义的异常,结尾自动插入分号
+// try...catch...finally
+// var 声明提升,赋值不提升
+// while
