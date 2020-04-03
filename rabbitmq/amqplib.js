8c40553a5269ec82dd7b9b78776e93e1c9f65e39
1372363493@qq.com <1372363493@qq.com>
2020-04-03 12:57:48 +0800

    auto commit

--- rabbitmq/amqplib.js
+++ rabbitmq/amqplib.js
@@ -38,4 +38,76 @@
 
 // channel.purgeQueue(queue) 清除队列内的消息,已经被处理的不清除
 
-// 
+// channel.bindQueue(queue, source, pattern, [args]) 将队列queue以路由pattern绑定到source上
+
+// channel.bindQueue(queue, source, pattern, [args]) 解绑
+
+// channel.assertQueue([queue, [options]]) 确保交换机存在(不存在则创建),初始化用的
+
+// channel.assertExchange(exchange, type, [options]) 确保交换机存在(不存在则创建),初始化用的
+// exchange 交换机名字
+// type 类型: direct完全匹配，topic模糊匹配(慢一些),Fanout不校验路由键直接发,headers根据头属性进行完全匹配
+// durable 持久化,默认为true
+// internal 内部的，不可被publish
+// autoDelete 有过queue且所有queue解绑则删除,默认false
+// alternateExchange 可选的交换机，如果当前交换机无匹配队列则转发到这个交换机上
+// 返回{ exchange: 'exchangeName' }
+
+// channel.checkExchange() check交换机(交换机不存在会报错)
+
+// channel.deleteExchange(exchange, [options]) 删除交换机(交换机不存在会报错)
+// ifUnused 还有绑定队列不删除
+
+// channel.bindExchange(destination, source, pattern, [args]) 以指定routeKey将交换机绑到另一台，提供了编排exchange的能力
+
+// channel.unbindExchange(destination, source, pattern, [args]) 解绑
+
+// channel.publish(exchange, routingKey, content, [options])
+// expiration 毫秒字符串后自动过期
+// userId 保持打开连接时的username一致
+// CC 其余的routeKey数组,抄送
+// priority 正整数表示优先级
+// persistent 消息持久化
+// deliveryMode 持久化1为假2为真，同上,不推荐
+// mandatory 如果没有找到routingKey相应的队列,则返还给生产者
+// BCC 类似于CC,密送，不包含服务端信息
+// immediate(deprecated) 队列没有消费者的时候不会入队，会返回给生产者
+// contentType message的MIME类型
+// contentEncoding message的MIME encoding
+// headers 消息头
+// correlationId 相关性id,常用判断请求来源
+// replyTo 
+// messageId
+// timestamp
+// type
+// appId
+
+// channel.sendToQueue(queue, content, [options]) 绕过路由交换直接向队列发送消息
+
+// channel.consume(queue, msg => {}, [options]) 消费队列的消息
+
+// channel.cancel(consumerTag) 不向这个consumer发送消息
+
+// channel.get(queue, [options]) 从mq中消费一个消息
+
+// channel.ack(message, [allUpTo]) 确认收到消息
+
+// channel.ackAll() 确认频道中所有未完成的消息
+
+// channel.nack(message, [allUpTo, [requeue]]) 抛弃消息
+
+// channel.nackAll([requeue]) 抛弃频道中所有消息
+
+// channel.reject(message, [requeue]) 兼容v2.3.0之前版本nack
+
+// channel.prefetch(message, [requeue]) 设置每个频道同时获取消息的数量
+
+// channel.recover() 频道中所有未入队的队列重新入队
+
+// ConfirmChannel 生产者和broker确认机制，和channel一样，除了以下
+
+// cch.publish(exchange, routingKey, content, options, (err, ok) ={}) 多了一个callabck确认broker收到
+
+// cch.sendToQueue(queue, content, options, (err, ok) => {})
+
+// cch.waitForConfirms() cch队列所有的消息是否已经被broker收到，有一个nack就会报错
